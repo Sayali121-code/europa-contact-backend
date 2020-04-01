be69817f2315dfcfcfbaaa104fd7fd2f84131b32
Sayali More <sayali.more@coditas.com>
2020-04-01 19:11:20 +0530

    europa contact backend

---
+++ src/contacts/contacts/contacts.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Contact } from '../contact.entity';
+import { ContactsService } from '../contacts/contacts.service';
+
+@Controller('contacts')
+export class ContactsController {
+  constructor(private contactsService: ContactsService) {}
+
+  /*  @Get()
+  index(): Promise<Contact[]> {
+    return this.contactsService.findAll();
+  } */
+
+  @Post('create')
+  async create(@Body() ReservationData: Contact): Promise<any> {
+    return this.contactsService.create(ReservationData);
+  }
+}
